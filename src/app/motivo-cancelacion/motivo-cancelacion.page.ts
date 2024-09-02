b1cd63a1464e6bc3cae518dc6c8cb48e9cdaf703
Alan Barrera <alangabriel49@gmail.com>
2024-09-02 14:00:13 -0400

    paginapedorra

---
+++ src/app/motivo-cancelacion/motivo-cancelacion.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-motivo-cancelacion',
+  templateUrl: './motivo-cancelacion.page.html',
+  styleUrls: ['./motivo-cancelacion.page.scss'],
+})
+export class MotivoCancelacionPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
