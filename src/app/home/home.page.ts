4bfd122f08862eafc08b9ded2548e91839068207
CarlosAP1983 <carlos.arayac@icloud.com>
2024-10-14 15:27:11 -0300

    commit simple, sin cambios

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,39 +1,29 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { LoginService } from 'src/app/services/login.service';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
-  username: string = '';
-  password: string = '';
+export class HomePage {
+  nombreUsuario: string = '';
+  contrasena: string = '';
 
-  destination: string = ''; 
-  loginSrv = inject(LoginService);
-  
-  
+  destino: string = ''; 
+  LoginService = inject(LoginService);
 
   constructor(private navCtrl: NavController) {}
 
-  //Metodo del ciclo de vida de Angular, se llama una vez que la vista se inicializa
-  ngOnInit() {}
-
-  //Metodo para manejar el inicio de sesion
-  login() {
-   
-      this.loginSrv.login(this.username,this.password);
-      this.navCtrl.navigateForward('/seleccion-perfil'); //Si tiene cuenta pasa a la vista seleccion-perfil
-      
-    } 
-  
-
-  //Metodo para pasar a la pagina registro
-  goToRegister() {
-    this.navCtrl.navigateForward('/registro'); //Pasa a la vista registro
+  // Método para manejar el inicio de sesión
+  iniciarSesion() {
+    this.LoginService.login(this.nombreUsuario, this.contrasena);
+    this.navCtrl.navigateForward('/seleccion-perfil'); // Si tiene cuenta pasa a la vista seleccion-perfil
   }
-  
+
+  // Método para pasar a la página de registro
+  irARegistro() {
+    this.navCtrl.navigateForward('/registro'); // Pasa a la vista registro
+  }
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -8,19 +8,16 @@ export class LoginService {
   NombreUsuario: string = '';
   logeado:boolean = false;
 
-
   constructor() { }
 
   public login(user:string,password:string){
-
+    //manejar la sesion
     if(user.length > 1 && password =='123'){
       this.logeado = true;
       this.NombreUsuario = user;
 
     }else {
-
       throw{};
     }
-
   }
 }
