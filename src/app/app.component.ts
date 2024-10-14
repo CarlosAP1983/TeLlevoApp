013dda7fffaa26a130ceb6208678027cdb7d365b
Alan Barrera <alangabriel49@gmail.com>
2024-10-14 17:55:23 -0300

    registro firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Capacitor } from '@capacitor/core';
-import { initializeApp } from "firebase/app";
+
 
 @Component({
   selector: 'app-root',
@@ -10,26 +10,5 @@ import { initializeApp } from "firebase/app";
 export class AppComponent {
   constructor() {}
 
-  async NgOnInit(){
-    //veremos si no estamos en un celular
-    if(!Capacitor.isNativePlatform()){
-
-
-      const firebaseConfig = {
-
-        apiKey: "AIzaSyDpDoVzzvdf_ZxIvnUUR7CZDNm41pwwnuE",      
-        authDomain: "tellevoapp-aba96.firebaseapp.com",      
-        projectId: "tellevoapp-aba96",      
-        storageBucket: "tellevoapp-aba96.appspot.com",      
-        messagingSenderId: "682925780025",      
-        appId: "1:682925780025:web:50ca71adb8fab01f07f1e8"
-      
-      };
-            
-      // Initialize Firebase      
-      initializeApp(firebaseConfig);
-      console.log()
-    }
-  }
-
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,24 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp } from "firebase/app";
+import { Capacitor } from '@capacitor/core';  
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpDoVzzvdf_ZxIvnUUR7CZDNm41pwwnuE",
+  authDomain: "tellevoapp-aba96.firebaseapp.com",
+  projectId: "tellevoapp-aba96",
+  storageBucket: "tellevoapp-aba96.appspot.com",
+  messagingSenderId: "682925780025",
+  appId: "1:682925780025:web:50ca71adb8fab01f07f1e8"
+};
+
+
+if (!Capacitor.isNativePlatform()) {
+  initializeApp(firebaseConfig);
+  console.log('Firebase inicializado para plataforma web.');
+}
 
 @NgModule({
   declarations: [AppComponent],
